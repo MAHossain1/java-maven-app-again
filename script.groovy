8787c13134fe2f1921589dc596f43042e04e5f64
MAHossain1 <mahpro110@gmail.com>
2025-03-08 16:24:47 +0600

    Configure version increment in build

--- script.groovy
+++ script.groovy
@@ -1,15 +1,15 @@
 
 def buildJar() {
     echo 'Building JAR..'
-   sh 'mvn package -U'
+    sh 'mvn clean package -U'
 } 
 
 def buildImage() {
     echo 'building the docker image..'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t arman04/java-maven-app:jma-3.1.0 .'
+        sh "docker build -t arman04/java-maven-app:${IMAGE_NAME} ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push arman04/java-maven-app:jma-3.1.0'
+        sh "docker push arman04/java-maven-app:${IMAGE_NAME}"
     }
     echo 'docker image built and pushed to docker hub'
 } 
