3ce096abb36a4815fc317e00693d7b68ef3541ae
MAHossain1 <mahpro110@gmail.com>
2025-03-08 12:46:20 +0600

    update docker imagename

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildJar() {
 def buildImage() {
     echo 'building the docker image..'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t arman04/java-maven-app:jma-3.0 .'
+        sh 'docker build -t arman04/java-maven-app:jma-3.1.0 .'
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push arman04/java-maven-app:jma-3.0'
+        sh 'docker push arman04/java-maven-app:jma-3.1.0'
     }
 } 
 
